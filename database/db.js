9729b547eae30df36f99eb75b044e3ddbea5d5c3
menep <menep@protonmail.com>
2020-12-06 18:01:58 +0100

    Change path to store database, add columns in 'todos' table

--- database/db.js
+++ database/db.js
@@ -1,9 +1,13 @@
 const sqlite = require("sqlite3").verbose();
 
 module.exports.init = () => {
-	const db = new sqlite.Database("test.sql");
+	const db = new sqlite.Database("./database/todos.sql");
+
 	db.serialize(() => {
-		const sql = `CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, title VARCHAR(140))`;
+		const sql = `
+			CREATE TABLE IF NOT EXISTS todos 
+			(id INTEGER PRIMARY KEY, title VARCHAR(140), description TEXT, due DATE, completed BOOLEAN, created DATETIME, updated DATETIME, deleted DATETIME)
+		`;
 		db.run(sql);
 	});
 };
