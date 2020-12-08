1f98d23b4e6727da58f38d72517ae314bae18e34
menep <menep@protonmail.com>
2020-12-08 14:19:46 +0100

    Add render logic for home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const Todo = require("../models/Todo");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-	res.render("index", { title: "Express" });
+	Todo.all((err, rows) => {
+		const todos = rows.map((el) => {
+			el.completed = !!el.completed;
+
+			return el;
+		});
+
+		res.render("index", { title: "Express", todos });
+	});
 });
 
 module.exports = router;
