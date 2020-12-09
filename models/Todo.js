2b97168a67bde911f98b74faea6b77bcc08c6e85
menep <menep@protonmail.com>
2020-12-09 12:34:46 +0100

    Add constructor and save methods to Todo model

--- models/Todo.js
+++ models/Todo.js
@@ -1,9 +1,25 @@
 const db = require("../database/db.js").connect();
 
 class Todo {
+	constructor(obj) {
+		for (const property in obj) {
+			this[property] = obj[property];
+		}
+	}
+
 	static all(cb) {
 		const sql = "SELECT * FROM todos";
 		db.all(sql, cb);
+	}
+
+	save(cb) {
+		const sql = `INSERT INTO todos (
+			title, due_at, created_at
+		) VALUES (
+			:title, :due_at, datetime('now')
+		)`;
+
+		db.run(sql, this.title, this.due_at, cb);
 	}
 }
 
