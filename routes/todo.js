8b80e5cccba7275695d3543420907f58717a50a5
menep <menep@protonmail.com>
2020-12-09 12:36:08 +0100

    Add '/todo' POST route to save new todo

--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Todo = require("../models/Todo");
 
 router.get("/", (req, res, next) => {
 	res.render("add-todo");
+});
+
+router.post("/", (req, res, next) => {
+	const title = req.body.title;
+	const due_at = req.body.due_at;
+	const todo = new Todo({ title, due_at });
+
+	todo.save((err) => {
+		if (err) {
+			console.log(err);
+		}
+		res.redirect("/");
+	});
 });
 
 module.exports = router;
