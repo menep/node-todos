d69eacdf376e04aab8a62eda9d5e69d7902f96f3
menep <menep@protonmail.com>
2020-12-14 17:04:46 +0100

    Rename '/todo' POST route to '/'

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,17 @@ router.get("/new", (req, res, next) => {
 	res.render("new");
 });
 
+router.post("/", (req, res, next) => {
+	const title = req.body.title;
+	const due_at = req.body.due_at;
+	const todo = new Todo({ title, due_at });
+
+	todo.save((err) => {
+		if (err) {
+			console.log(err);
+		}
+		res.redirect("/");
+	});
+});
+
 module.exports = router;
--- routes/todo.js
+++ routes/todo.js
@@ -1,18 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const Todo = require("../models/Todo");
-
-router.post("/", (req, res, next) => {
-	const title = req.body.title;
-	const due_at = req.body.due_at;
-	const todo = new Todo({ title, due_at });
-
-	todo.save((err) => {
-		if (err) {
-			console.log(err);
-		}
-		res.redirect("/");
-	});
-});
 
 module.exports = router;
