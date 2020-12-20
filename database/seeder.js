7788e6e3dc46987d9e7094a3a431df5523672d3c
menep <menep@protonmail.com>
2020-12-20 11:37:13 +0100

    Use 'formatDateYYYYMMDD' in seeder to format dates

--- database/seeder.js
+++ database/seeder.js
@@ -1,14 +1,13 @@
 const db = require("./db");
 const faker = require("faker");
+const datetime = require("../utils/datetime.js");
 const dbConnection = db.connect();
-
-const getUnixTs = (tsMs) => Math.floor(tsMs / 1000);
 
 const seedRow = () => {
 	const title = faker.lorem.words(5);
-	const due_at = getUnixTs(faker.date.future(0).getTime());
+	const due_at = datetime.formatDateYYYYMMDD(faker.date.future(0));
 	const completed = faker.random.boolean();
-	const created_at = getUnixTs(faker.date.past(0).getTime());
+	const created_at = datetime.formatDateYYYYMMDD(faker.date.past(0));
 
 	var stmt = dbConnection.prepare(`INSERT INTO todos (title, due_at, completed, created_at, updated_at, deleted_at) VALUES 
 		(?, ?, ?, ?, NULL, NULL)
